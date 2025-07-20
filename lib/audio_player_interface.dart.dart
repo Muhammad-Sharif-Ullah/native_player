2375ddbb76d451186ee0545d643519b68e163ae3
Muhammad-Sharif-Ullah <sharifnkbd@gmail.com>
2025-07-20 18:19:56 +0600

    pause, stop, seek added in dart side

--- lib/audio_player_interface.dart.dart
+++ lib/audio_player_interface.dart.dart
@@ -21,14 +21,40 @@ class AudioPlayerInterfaceDart {
   }
 
   // Method to play audio from a URL
-  Future<void> play(String url) async {}
+  Future<void> play(String url) async {
+    try {
+      await platform.invokeMethod('play', {'url': url});
+    } on PlatformException catch (e) {
+      log("Failed to play audio: '${e.message}'.");
+    }
+  }
 
   // Method to pause the audio playback
-  Future<void> pause() async {}
+  Future<void> pause() async {
+    try {
+      await platform.invokeMethod('pause');
+    } on PlatformException catch (e) {
+      log("Failed to pause audio: '${e.message}'.");
+    }
+  }
 
   // Method to stop the audio playback
-  Future<void> stop() async {}
+  Future<void> stop() async {
+    try {
+      await platform.invokeMethod('stop');
+    } on PlatformException catch (e) {
+      log("Failed to stop audio: '${e.message}'.");
+    }
+  }
 
   // Method to seek to a specific position in the audio
-  Future<void> seek(Duration position) async {}
+  Future<void> seek(Duration position) async {
+    try {
+      await platform.invokeMethod('seekTo', {
+        'position': position.inMilliseconds,
+      });
+    } on PlatformException catch (e) {
+      log("Failed to seek audio: '${e.message}'.");
+    }
+  }
 }
